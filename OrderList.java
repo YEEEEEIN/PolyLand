7a8879a5952219c1be1b6641d2e1f8a8a77256c5
DESKTOP-99T6GRM\예인 <dpdlssla12@naver.com>
2023-04-26 16:03:43 +0900

    주문리스트 클래스

---
+++ OrderList.java
@@ -0,0 +1,67 @@
+package PolyLand;
+
+public class OrderList { // getter, setter ¸¸µé±â
+	int ticket;
+	int age;
+	int price;
+	int count;
+	int first;
+	double discount;
+	double total;
+
+	public double getTotal() {
+		return total;
+	}
+
+	public void setTotal(double total) {
+		this.total = total;
+	}
+
+	public int getTicket() {
+		return ticket;
+	}
+
+	public void setTicket(int ticket) {
+		this.ticket = ticket;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public int getCount() {
+		return count;
+	}
+
+	public void setCount(int count) {
+		this.count = count;
+	}
+
+	public int getFirst() {
+		return first;
+	}
+
+	public void setFirst(int first) {
+		this.first = first;
+	}
+
+	public double getDiscount() {
+		return discount;
+	}
+
+	public void setDiscount(double discount) {
+		this.discount = discount;
+	}
+}
